cb20a40fe79e917d3fb7c1e68257da7cfb0b656d
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2023-10-22 00:36:13 +0500

    count down timer final
    finished count down timer and added one new package

--- TypeScript  Projects/CountDown_Timer/index.js
+++ TypeScript  Projects/CountDown_Timer/index.js
@@ -1,4 +1,7 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
+import { differenceInSeconds } from "date-fns";
 const sleep = () => {
     return new Promise((res) => {
         setTimeout(res, 2000);
@@ -10,3 +13,38 @@ async function welcome() {
     greetings.stop();
 }
 await welcome();
+const res = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userInput",
+        message: chalk.red.bold("Please enter the seconds"),
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter a valid Seconds";
+            }
+            else if (input > 60) {
+                return "Seconds must be in 60";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+]);
+let input = res.userInput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log('Timer has expired');
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor((timeDiff % 60));
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }, 1000);
+}
+startTime(input);
